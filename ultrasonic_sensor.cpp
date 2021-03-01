94bea945b792f5334660febd8f35a22dd1eb61ff
prashant737 <64883131+prashant737@users.noreply.github.com>
2021-03-02 01:32:13 +0530

    Create ultrasonic_sensor.cpp

---
+++ ultrasonic_sensor.cpp
@@ -0,0 +1,40 @@
+#define trigPin 13
+#define echoPin 12
+#define onepin 11
+void setup() 
+{
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(onepin,OUTPUT);
+}
+void loop() 
+{
+  long duration, distance;
+  digitalWrite(trigPin, LOW);        
+  delayMicroseconds(2);              
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);           
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance >= 2000 || distance <= 0)
+  {
+    Serial.println("Out of range");
+  }
+  else 
+  {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  if (distance<=10)
+  {
+      digitalWrite(onepin,HIGH);
+  }
+  else
+  {
+    digitalWrite(onepin,LOW);
+  }
+  
+  delay(500);
+}
